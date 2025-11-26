8f0b9933309d3d8037f8921f3230f13474331812
Your Name <you@example.com>
2025-11-26 12:22:32 +0800

    Added some navigation logic that might work once the communication layer works, using the values from the subscriber to inform navigational decision making. Unfortunately still deeply broken

--- wall_avoider/wall_avoider_node.py
+++ wall_avoider/wall_avoider_node.py
@@ -29,44 +29,64 @@ class WallAvoider(Node):
         self.sensor0_subscription = self.create_subscription(
             FloatStamped,
             '/ps7',
-            self.side_sensor_1_callback,
+            self.side_sensor_0_callback,
             10
         )
+        self.sensor0_subscription
 
         self.sensor1_subscription = self.create_subscription(
             FloatStamped,
-            '/ps0',
-            self.side_sensor_2_callback,
+            '/ps6',
+            self.side_sensor_1_callback,
             10
         )
-        
-        # Publisher for movement commands already created above as self.cmd_pub
+        self.sensor1_subscription
 
-        #TODO: Create timer
+        #TODO: Create timers
         timer_period = 0.5
-        self.timer = self.create_timer(timer_period, self.control_callback)
+        self.control_timer = self.create_timer(timer_period, self.control_callback)
 
         self.get_logger().info('Wall avoider node started')
 
-    def side_sensor_1_callback(self, msg):
+    def side_sensor_0_callback(self, msg):
         # TODO: check which sensor this is
         self.sensor_array[0] = msg.data
-        pass
+        self.get_logger().info(f"sensor_1 received {msg.data}")
 
-    def side_sensor_2_callback(self, msg):
+    def side_sensor_1_callback(self, msg):
         # TODO: check which sensor this is
         self.sensor_array[1] = msg.data
-        pass
+        self.get_logger().info(f"sensor_2 received {msg.data}")
 
     def control_callback(self):
         # TODO: implement wall avoidance logic using sensor array
         # test message for now
+
+        # initialise message
         twist_msg = TwistStamped()
-        twist_msg.twist.linear.x = self.maximum_speed
-        twist_msg.twist.angular.z = self.turning_speed
+
+        # do robot logic (check which one is left and right)
+        # if both sensors too close reverse
+        if self.sensor_array[0] and self.sensor_array[1] < self.avoidance_threshold:
+            twist_msg.twist.linear.x = -self.maximum_speed
+
+        # if one sensor too close turn a mystery direction
+        elif self.sensor_array[0] < self.avoidance_threshold:
+            twist_msg.twist.linear.x = self.maximum_speed/2
+            twist_msg.twist.angular.z = self.turning_speed
+
+        # if other sensor too close turn a second mystery direction
+        elif self.sensor_array[1] :
+            twist_msg.twist.linear.x = self.maximum_speed/2
+            twist_msg.twist.angular.z = -self.turning_speed
+
+        # otherwise go in a straight line full speed
+        else:
+            twist_msg.twist.linear = self.maximum_speed
+
+        # publish message for robot
         self.cmd_pub.publish(twist_msg)
         self.get_logger().info(f'Publishing: linear.x={twist_msg.twist.linear.x}, angular.z={twist_msg.twist.angular.z}')
-        pass
 
 def main():
     rclpy.init()
